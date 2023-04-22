c1d7418cdcb2190312b23599ca2eca7b2b9e0a89
Brandon Howard <brandon@WIN-08C8BRG64OI.home>
2023-04-22 13:29:09 -0700

    Main bulk of code, general removal of old code

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -30,30 +30,26 @@ export default function Home() {
                   className=" cursor-pointer text-2xl dark:text-gray-200"
                 />
               </li>
-              <li>
-                <a
-                  href="resume"
-                  className="bg-gradient-to-r from-emerald-green to-peacock-blue px-4 py-2 border-none rounded-md ml-8 text-xl text-black"
-                >
-                  Resume
-                </a>
-              </li>
             </ul>
           </nav>
           <div className="text-center p-10 py-10">
             <h2 className="text-5xl py-2 text-peacock-blue font-medium md:text-6xl dark:text-emerald-green">
-              Brandon Howard
+              Resume
             </h2>
-            <h3 className="text-2xl py-2 md:text-3xl dark:text-gray-200 text-gray-800">
-              Student and Future IT Professional
-            </h3>
-            <p className="text-md py-5 leading-8 text-gray-800 max-w-xl mx-auto md:text-xl dark:text-gray-200">
-              Current student learning the ropes to IT. Look below at some of my
-              projects and ramblings.
-            </p>
           </div>
-          <div className="flex flex-col gap-10">
-            <div className="basis-1/3 flex-1 rounded-2xl text-center shadow-lg p-10 transition ease-in-out delay-30 bg-peacock-blue hover:-translate-y-1 hover:scale-110 hover:bg-emerald-green duration-300"></div>
+          <div className="flex basis-1/3 rounded-2xl justify-center p-10 sahdow-lg bg-peacock-blue ">
+            <object
+              data="/assets/resume.pdf"
+              type="application/pdf"
+              className="object-cover"
+              width="75%"
+              height="500px"
+            >
+              <p>
+                Unable to display PDF file.
+                <a href="/assets/resume.pdf">Download instead.</a>
+              </p>
+            </object>
           </div>
         </section>
       </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
+  darkMode: 'class',
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    extend: {},
+    colors: {
+      'emerald-green': '#09BC8A',
+      'peacock-blue': '#005F69',
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
     },
   },
   plugins: [],
